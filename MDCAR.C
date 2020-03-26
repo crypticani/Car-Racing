ffa7036b2de23d0cb25441bba77e19c3f0288e15
Aniket Kumar <44335648+Aniketkjnv@users.noreply.github.com>
2020-03-26 15:25:57 +0530

    Update MDCAR.C

--- MDCAR.C
+++ MDCAR.C
@@ -7,21 +7,21 @@ void main()
 	clrscr();
 	initgraph(&gd,&gm,"C:\\TC\\BGI");
 
-	rectangle(250,250,270,280); //roof
-	rectangle(246,280,274,285);
-	rectangle(240,240,280,295); //main outline
+	rectangle(250,350,270,380); //roof
+	rectangle(246,380,274,385);
+	rectangle(240,340,280,395); //main outline
 
-	line(240,240,250,250);         //top_left
-	line(270,250,280,240);         //top_right
-	line(274,285,280,295);         //bottom_left
-	line(246,285,240,295);         //bottom_right
+	line(240,340,250,350);         //top_left
+	line(270,350,280,340);         //top_right
+	line(274,385,280,395);         //bottom_left
+	line(246,385,240,395);         //bottom_right
 
-	ellipse(260,240,0,180,20,30);     //bonet
-	ellipse(260,295,180,360,20,10);   //trunk_lid
+	ellipse(260,340,0,180,20,30);     //bonet
+	ellipse(260,395,180,360,20,10);   //trunk_lid
 
-	arc(260,255,10,170,24);           //side_mirror
-	arc(250,213,250,360,5);      //left_front_light
-	arc(270,213,180,300,5);     //right_front_light
+	arc(260,355,10,170,24);           //side_mirror
+	arc(250,313,250,360,5);      //left_front_light
+	arc(270,313,180,300,5);     //right_front_light
 	getch();
 	closegraph();
 }
