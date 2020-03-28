a1aa0305541c86e8dc670e190c8cbd2b3990697a
Aniket Kumar <44335648+Aniketkjnv@users.noreply.github.com>
2020-03-28 10:42:15 +0530

    Update HEART_SH.C

--- HEART_SH.C
+++ HEART_SH.C
@@ -4,12 +4,16 @@
 void main()
 {
 	int gd=DETECT,gm;
-	int h[]={57,75,46,60,50,50,55,51,57,60,60,51,65,50,69,60,57,75};
-	clrscr();
-	initgraph(&gd,&gm,"C:\\TC\\BGI");
+	int h1[]={517,75,506,60,510,50,515,51,517,60,520,51,525,50,529,60,517,75};
+	int h2[]={557,75,546,60,550,50,555,51,557,60,560,51,565,50,569,60,557,75};
+	int h3[]={597,75,586,60,590,50,595,51,597,60,600,51,605,50,609,60,597,75};
+	
+	initgraph(&gd,&gm,"C:\\Tc\\BGI");
 	setcolor(RED);
 	setfillstyle(SOLID_FILL,RED);   //try EMPTY_FILL to draw just red outline.
-	fillpoly(9,h);
+	fillpoly(9,h1);
+	fillpoly(9,h2);
+	fillpoly(9,h3);
 
 	getch();
 	closegraph();
